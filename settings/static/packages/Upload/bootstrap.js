b375f2c627b0f6f8f5230d957d6f2b1d71739b13
NextSeason <kelcb@163.com>
2015-07-10 16:29:20 +0800

    upgrade

--- settings/static/packages/Upload/bootstrap.js
+++ settings/static/packages/Upload/bootstrap.js
@@ -1,20 +1,204 @@
 J.Package( {
     initialize : function( options ) {
         this.bindEvent();
+
+        this.boxSize = {
+            w : 240,
+            h : 240
+        };
+
+        this.dragging = false;
+        this.minSize = {};
+        this.maxSize = {};
+        this.currentSize = null;
+        this.currentPosition = {};
+
+        this.img = null;
+        this.imgSize = {};
     },
 
     bindEvent : function() {
         var me = this,
-            btn = $( '.real-file-btn' );
+            btn = $( '.real-file-btn' ),
+            uploadDialog = $( '#upload-dialog' );
 
         btn.on( 'change', function( e ) {
             var files = $( this ).get( 0 ).files;
 
-            if( files.length ) {
-                me.showDialog();
+            if( !files.length ) return false;
+
+            me.readFile( files[ 0 ] );
+            me.showDialog();
+            $( this ).val( '' );
+        } );
+
+        uploadDialog.find( 'input.resize-bar' ).on( 'input change', function( e ) {
+            var val = $( this ).val();            
+            me.resize( val / 100 * 2 + 1 );
+        } );
+
+        uploadDialog.find( '.cancel' ).on( 'click', function( e ) {
+            e.preventDefault();
+            uploadDialog.find( '.img-area .inner' ).html( '' );
+            uploadDialog.hide();
+        } );
+
+        uploadDialog.find( '.img-cover' ).on( 'mousedown', function( e ) {
+            me.dragging = true;
+            me.dragPoint = {
+                x : e.clientX,
+                y : e.clientY
+            };
+        } );
+
+        uploadDialog.on( 'mousemove', function( e ) {
+            if( !me.dragging ) return;
+            var x = e.clientX,
+                y = e.clientY;
+
+            var left = x - me.dragPoint.x + me.currentPosition.x,
+                top = y - me.dragPoint.y + me.currentPosition.y;
+
+            me.reposition( left, top );
+
+        } );
+
+        uploadDialog.on( 'mouseup', function( e ) {
+            if( me.dragging ) {
+                var x = e.clientX,
+                    y = e.clientY;
+
+                me.currentPosition = {
+                    x : x - me.dragPoint.x + me.currentPosition.x,
+                    y : y - me.dragPoint.y + me.currentPosition.y
+                };
+
+                me.dragging = false;
             }
         } );
     },
+
+    readFile : function( file ) {
+        var me = this;
+        var reader = new FileReader();
+
+        reader.onloadend = function( e ) {
+            var res = e.target.result,
+                w, h,
+                minWidth, minHeight,
+                img = $( '<img src=' + res + ' />' );
+
+            me.img = img;
+
+            $( '#upload-dialog .img-area .inner' ).html( '' ).append( img );
+
+            w = img.width();
+            h = img.height();
+
+            me.imgSize = {
+                w : w,
+                h : h 
+            };
+
+            if( w < me.boxSize.w && h < me.boxSize.h ) {
+                me.minSize = {
+                    w : w,
+                    h : h
+                }
+            } else {
+                if( w == h ) {
+                    me.minSize = {
+                        w : me.boxSize.w,
+                        h : me.boxSize.h
+                    };
+                } else if( w > h ) {
+                    minWidth = me.boxSize.w;
+                    minHeight = minWidth * h / w;
+                    me.minSize = {
+                        w : minWidth,
+                        h : minHeight
+                    };
+                } else {
+                    minHeight = me.boxSize.h;
+                    minWidth = w / h * minHeight;
+                    me.minSize = {
+                        w : minWidth,
+                        h : minHeight
+                    }
+                }
+            }
+
+            me.currentSize = {
+                w : me.minSize.w,
+                h : me.minSize.h
+            };
+
+            me.maxSize = {
+                w : me.minSize.w * 2,
+                h : me.minSize.h * 2
+            }
+
+            me.center();
+            me.setSize();
+        };
+
+        reader.readAsDataURL( file );
+    },
+
+    resize : function( scale ) {
+        this.img.css( 'transform', 'scale(' + scale + ')' );
+    },
+
+    setSize : function( w, h ) {
+        var w = this.minSize.w,
+            h = this.minSize.h;
+        this.img.css( 'width', w ).css( 'height', h );
+    },
+
+    reposition : function( left, top ) {
+        this.img.css( 'left', left ).css( 'top', top );
+    },
+
+    center : function() {
+        var left = ( this.boxSize.w - this.currentSize.w ) / 2,
+            top = ( this.boxSize.h - this.currentSize.h ) / 2;
+
+        this.currentPosition = {
+            x : left,
+            y : top
+        };
+
+        this.reposition( left, top );
+    },
+
+    getAdjustedImg : function() {
+    },
+
+    upload : function() {
+        var formData = new FormData();
+
+        formData.append( 'file', file );
+
+        var xhr_provider = function() {
+            var xhr = $.ajaxSettings.xhr();
+
+            if( xhr.upload ) {
+                xhr.upload.addEventListener( 'progress', function( e ) {
+                }, false );
+            }
+            return xhr;
+        };
+
+        $.ajax( {
+            url: '/common/interface/upload',
+            type : 'post',
+            data: formData,
+            contentType: false,
+            processData: false,
+            xhr: xhr_provider,
+        } );
+    },
     showDialog : function() {
+        $( '#upload-dialog' ).show();
     }
 } );
--- user/static/packages/List/bootstrap.js
+++ user/static/packages/List/bootstrap.js
@@ -1,10 +1,5 @@
 J.Package( {
     initialize : function( options ) {
-        this._cache = {
-            preloadIds : [],
-            posts : {}
-        };
-
         this.compiledTpl = J.template( $( '#post-list-tpl' ).val() );
 
         this.page = $( 'ul.nav .focus' ).attr( 'data-page' );
@@ -25,8 +20,7 @@ J.Package( {
     load : function( pn ) {
 
         var me = this,
-            rn = 20,
-            preloadIds = this._cache.preloadIds;
+            rn = 20;
 
         var start = ( pn - 1 ) * rn;
 
@@ -49,9 +43,7 @@ J.Package( {
 
             var posts = me.formatData( response.data.posts );
             
-            me.render( response.data.posts );
-
-            me.setCache( response );
+            me.render( posts );
         } );
 
     },
@@ -89,184 +81,6 @@ J.Package( {
         $( '.post-list' ).html( html );
     },
 
-    getPostEl : function( el ) {
-        var postEl = el.closest( 'li.posts' );
-
-        if( !postEl.length ) return null;
-
-        return postEl;
-    },
-
-    getPostId : function( postEl ) {
-        return postEl.attr( 'data-post-id' );
-    },
-
-    setCache : function( response ) {
-    },
     bindEvent : function() {
-        var me = this;
-        $( '.topic-area .sort' ).on( 'click', function( e ) {
-            var order = $( this ).attr( 'data-order' );
-            $( '.post-list' ).html( '' );
-            me.load( order, 0 );
-            $( '.list-area .sort' ).removeClass( 'focus' );
-            $( this ).addClass( 'focus' );
-            $( '.loading.list-top' ).show();
-        } );
-
-        $( '.topic-area' ).on( 'click', '.op-btn', function( e ) {
-            var action = $( this ).attr( 'data-action' );
-            me[ action + 'Action' ]( $( this ) );
-        } );
-
-        $( '.topic-area' ).on( 'click', '.bubbles .close', function( e ) {
-            $( this ).closest( '.bubbles' ).hide();
-        } );
-
-        $( '.topic-area' ).on( 'click', '.report-submit', function( e ) {
-            me.submitReport( $( this ) );
-        } );
-    },
-
-    submitReport : function( el ) {
-        var postEl = this.getPostEl( el ),
-            postId = postEl.attr( 'data-post-id' ),
-            reportEl = el.closest( '.report' ),
-            tipEl = el.parent().find( '.tip' );
-
-        var checked = reportEl.find( 'input[type=radio]:checked' );
-
-        if( !checked.length ) {
-            tipEl.html( '请选择投诉原因' ).addClass( 'err' ); 
-            return;
-        }
-
-        var reason = checked.val();
-
-        var desc = $.trim( reportEl.find( 'textarea.desc' ).val() );
-
-        $.ajax( {
-            url : '/topic/interface/report',
-            method : 'POST',
-            data : {
-                post_id : postId,
-                reason : reason,
-                desc : desc
-            }
-        } ).done( function( response ) {
-            var errno = +response.errno;
-
-            if( !errno ) {
-                tipEl.removeClass( 'err' ).html( '您的投诉已经提交，我们会尽快为您处理。感谢您的支持' ); 
-
-                return;
-            }
-            switch( errno ) {
-                case 3 : 
-                    // not login
-                    break;
-                case 14 : 
-                    tipEl.html( '您已经提交过投诉，我们会尽快为您处理' ).addClass( 'err' ); 
-                    break;
-                default :
-                    tipEl.html( '系统错误，请稍候再试' ).addClass( 'err' ); 
-                    break;
-                
-            }
-
-        } );
-    },
-
-    agreeAction : function( el ) {
-        var postEl = this.getPostEl( el ),
-            postId = postEl.attr( 'data-post-id' ),
-            opinion = el.attr( 'data-intent' );
-
-        $.ajax( {
-            url : '/topic/interface/vote',
-            method : 'POST',
-            data : {
-                post_id : postId,
-                opinion : opinion,
-                type : 0,
-                value : 1
-            }
-        } ).done( function( response ) {
-            var errno = +response.errno,
-                o;
-
-            if( errno ) { return false; }
-
-            o = +el.find( 'b' ).html();
-
-            el.find( 'b' ).html( o + 1 );
-
-        } );
-    },
-
-    markAction : function( el ) {
-        var me = this,
-            postEl = this.getPostEl( el ),
-            postId = postEl.attr( 'data-post-id' ),
-            markId = +el.attr( 'data-mark-id' ),
-            data = {
-                post_id : postId,
-                act : +!markId,
-                mark_id : markId
-            };
-
-        if( markId ) {
-            el.removeClass( 'op-unmark' ).addClass( 'op-mark' ).attr( 'data-mark-id', 0 );
-            el.html( '<i class="fa fa-star-o"></i> 收藏' );
-            if( me.showingBubble( el ) == 'mark' ) me.hideBubble( el );
-        }
-
-        $.ajax( {
-            url : '/topic/interface/mark',
-            method : 'POST',
-            data : data
-        } ).done( function( response ) {
-            var errno = +response.errno,
-                data = response.data || {};
-
-            if( errno ) {
-            }
-
-            if( +data.mark ) {
-                el.removeClass( 'op-mark' ).addClass( 'op-unmark' ).attr( 'data-mark-id', data.mark );
-                el.html( '<i class="fa fa-star"></i> 取消收藏' );
-
-                me.showBubble( el );
-            }
-        } );
-
-    },
-    shareAction : function( el ) {
-        this.showBubble( el );
-    },
-
-    reportAction : function( el ) {
-        this.showBubble( el );
-    },
-    showingBubble : function( el ) {
-        return this.getPostEl( el ).find( '.op-bubbles' ).attr( 'data-showing' );
-    },
-    showBubble : function( el ) {
-        var postEl = this.getPostEl( el ),
-            bubbleEl = postEl.find( '.op-bubbles' ),
-            action = el.attr( 'data-action' ),
-            pos = el.position();
-
-        bubbleEl.attr( 'data-showing', action );
-     
-        bubbleEl.find( '> .triangle' ).css( 'left', pos.left + el.width() / 2 - 10 );
-        bubbleEl.hide();
-        bubbleEl.find( '.box' ).hide();
-        bubbleEl.find( '.' + action ).show();
-        bubbleEl.show();
-    },
-
-    hideBubble : function( el ) {
-        this.getPostEl( el ).find( '.op-bubbles' ).hide();
     }
 } );
