94873c247047afce542ab865d6239084010c7b1a
NextSeason <kelcb@163.com>
2015-08-20 03:46:50 +0800

    upgrade

---
+++ messageMobile/static/packages/Messages/bootstrap.js
@@ -0,0 +1,12 @@
+J.Package( {
+    initialize : function( options ) {
+        this.accountId = options.accountId;
+        this.load();
+        this.bindEvent();
+    },
+    load : function() {
+    },
+    bindEvent : function() {
+        var me = this;
+    }
+} );
